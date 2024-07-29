13ce2bdb191649ed1578dded8300ae582920e3a0
renzhiyu200110 <2924908430@qq.com>
2024-07-29 12:23:42 +0800

    瑞吉外卖第一次提交

---
+++ src/test/java/regiee_take_out/RegieeTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package regiee_take_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegieeTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
