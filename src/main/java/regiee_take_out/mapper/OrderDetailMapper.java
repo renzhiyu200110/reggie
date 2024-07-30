e17188e7c7e87f86509a0a8c044ca792c653560a
renzhiyu200110 <2924908430@qq.com>
2024-07-30 17:51:53 +0800

    瑞吉外卖第一次提交

--- src/main/java/regiee_take_out/mapper/OrderDetailMapper.java
+++ src/main/java/regiee_take_out/mapper/OrderDetailMapper.java
@@ -1,8 +1,9 @@
 package regiee_take_out.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.itheima.reggie.entity.OrderDetail;
+
 import org.apache.ibatis.annotations.Mapper;
+import regiee_take_out.entity.OrderDetail;
 
 @Mapper
 public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
